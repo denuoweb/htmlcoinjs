7f46d1857b486d107ffbe0dc6a14721f84b288df
Jaron Rosenau <jaron.rosenau@gmail.com>
2021-03-12 17:11:52 -0800

    switch to htmlcoin

--- src/HtmlcoinRPC_test.ts
+++ src/HtmlcoinRPC_test.ts
@@ -5,7 +5,7 @@ import { assert } from "chai"
 import { rpc, assertThrow } from "./test"
 
 // import { } from "mocha"
-describe("QtumRPC", () => {
+describe("HtmlcoinRPC", () => {
   it("can make RPC call", async () => {
     const info = await rpc.rawCall("getblockchaininfo")
     assert.isNotEmpty(info)
@@ -47,12 +47,12 @@ describe("QtumRPC", () => {
     const p2pkhAddress = await rpc.fromHexAddress(
       "b22cbfd8dffcd4e0120279c2cc41315fac2335e2",
     )
-    assert.strictEqual(p2pkhAddress, "qZoV3RKeHaxKM5RnuZdA5bwoYTCH73QLrE")
+    assert.strictEqual(p2pkhAddress, "hZoV3RKeHaxKM5RnuZdA5bwoYTCH73QLrE")
   })
 
   it("can convert a p2pkh address to a hex address", async () => {
     const hexAddress = await rpc.getHexAddress(
-      "qZoV3RKeHaxKM5RnuZdA5bwoYTCH73QLrE",
+      "hZoV3RKeHaxKM5RnuZdA5bwoYTCH73QLrE",
     )
     assert.strictEqual(hexAddress, "b22cbfd8dffcd4e0120279c2cc41315fac2335e2")
   })
